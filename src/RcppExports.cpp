0d30d074ade165cffa1e330fc37491b55c0b5c25
Zhongtian Lin <zhongtian@wokera.ai>
2023-05-10 17:22:47 -0400

    trivial documentation and other updates

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // mmult
 NumericMatrix mmult(NumericMatrix m, NumericVector v);
 RcppExport SEXP _MIRTutils_mmult(SEXP mSEXP, SEXP vSEXP) {
